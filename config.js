a6666df9e906782e9afee012807131faf72e9f13
Tuandeptryz <86765944+Tuandeptryz@users.noreply.github.com>
2021-07-03 12:40:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Tamm à!',
+    introTitle: 'My Tamm à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hehe',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Em thichh aaaa <33',
+    desc: ' Yêu anh nhiều khônggg ',
+    btnYes: 'Nhiềuuuu <33',
     btnNo: 'Khôngggg =)))',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Tuann dzz <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <3',
     mess: 'Hehee. Anh yeu emm 😘😘',
-    messDesc: 'Bae ăn tối vui vẻ nhá :333.',
+    messDesc: 'Bae ngủ ngon :333.',
     btnAccept: 'Okelaaa <3',
     messLink: 'https://www.facebook.com/tammi0496' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
